33fd018eeea225a56c32f47d6008560dd02189bb
kanta <kanta510g@outlook.jp>
2022-11-23 17:15:34 +0900

    add sin, cos, e, !

--- main.js
+++ main.js
@@ -18,6 +18,14 @@ function onKey(e){
         input.value = "";
         return;
     }
+    if(btnval == "sin"){
+        input.value += "sin("
+        return;
+    }
+    if(btnval == "cos"){
+        input.value += "cos("
+        return;
+    }
     input.value += btnval;
 }
 
@@ -31,19 +39,19 @@ function enter(e){
 function isFormula(formula){
     //数式として正しいかを判別
     //文字の種類(ホワイトリスト)
-    if(formula.search(/^[\d|\+|\-|×|÷|\(|\)|\.|\^|π|√]*$/g) == -1){
+    if(formula.search(/^[\d|\+|\-|×|÷|\(|\)|\.|\^|π|√|e|!|sin|cos]*$/g) == -1){
         return false;
     }
     //先頭の記号
-    if(formula.search(/^[\+×÷\.\^]/) != -1){
+    if(formula.search(/^[\+×÷\.\^\!]/) != -1){
         return false;
     }
     //連続する記号
-    if(formula.search(/[\+\-×÷\(\^√][×\+÷\.\)\^]/) != -1){
+    if(formula.search(/[\+\-×÷\(\^√sinco][×\+÷\.\)\^\!]/) != -1){
         return false;
     }
     //マイナスの制限
-    if(formula.search(/[\+\-\.\^√][\-]/) != -1){
+    if(formula.search(/[\+\-\.\^√sinco][\-]/) != -1){
         return false;
     }
     //複数の小数点
@@ -51,7 +59,7 @@ function isFormula(formula){
         return false;
     }
     //末尾の記号
-    if(formula.search(/[\-\+×÷\^√]$/) != -1){
+    if(formula.search(/[\-\+×÷\^√sinco]$/) != -1){
         return false;
     }
     return true;
@@ -100,6 +108,22 @@ function hiddenMul(formula){
             break;
         }
     }
+    while(true){
+        result = formula.search(/\dC/);
+        if(result != -1){
+            formula = formula.slice(0, result+1) + "×" + formula.slice(result+1, formula.length);
+        }else{
+            break;
+        }
+    }
+    while(true){
+        result = formula.search(/\dS/);
+        if(result != -1){
+            formula = formula.slice(0, result+1) + "×" + formula.slice(result+1, formula.length);
+        }else{
+            break;
+        }
+    }
     return formula;
 }
 
@@ -107,7 +131,7 @@ function minus(formula){
     //二項演算子としての-を~に置換
     let result;
     while(true){
-        result = formula.search(/[\d\)]\-[\d\(]/);
+        result = formula.search(/[\d\)\!]\-[\d\(]/);
         if(result != -1){
             formula = formula.slice(0, result+1) + "~" + formula.slice(result+2, formula.length);
         }else{
@@ -129,9 +153,49 @@ function pi(formula){
     while(true){
         result = formula.search(/π/);
         if(result != -1){
-            formula = formula.slice(0, result) + "3.14" + formula.slice(result+1, formula.length)
+            formula = formula.slice(0, result) + "3.14159" + formula.slice(result+1, formula.length)
         }else{
             return formula
+        }
+    }
+}
+
+function napier(formula){
+    let result;
+    while(true){
+        result = formula.search(/\de|e\d/);
+        if(result != -1){
+            formula = formula.slice(0, result+1) + "×" + formula.slice(result+1, formula.length);
+        }else{
+            break;
+        }
+    }
+    while(true){
+        result = formula.search(/e/);
+        if(result != -1){
+            formula = formula.slice(0, result) + "2.71828" + formula.slice(result+1, formula.length)
+        }else{
+            return formula
+        }
+    }
+}
+
+function trigonometric_function(formula){
+    let result;
+    while(true){
+        result = formula.search(/cos/);
+        if(result != -1){
+            formula = formula.slice(0, result) + "C" + formula.slice(result+3, formula.length)
+        }else{
+            break;
+        }
+    }
+    while(true){
+        result = formula.search(/sin/);
+        if(result != -1){
+            formula = formula.slice(0, result) + "S" + formula.slice(result+3, formula.length)
+        }else{
+            return formula;
         }
     }
 }
@@ -170,11 +234,24 @@ function find_brackets(formula){
 function find_root(formula){
     let result = formula.lastIndexOf("√");
     if(result != -1){
-        let index = formula.slice(result+1, formula.length).search(/[\+\~×÷]/);
+        let index = formula.slice(result+1, formula.length).search(/[\+\~×÷\^\!]/);
         if(index == -1){
             return [result+1, formula.length]
         }
         return [result+1, index+result+1];
+    }
+    return -1;
+}
+
+function find_exp(formula){
+    let result = formula.search(/\!/);
+    if(result != -1){
+        for(let i = result-1; i >= 0; i -= 1){
+            if(formula[i].search(/[\*\+\~\×÷\/^]/) !== -1){
+                return [i+1, result];
+            }
+        }
+        return [0, result];
     }
     return -1;
 }
@@ -205,17 +282,42 @@ function div(formula1, formula2){
     return Number(calculate(formula1)) / n;
 }
 function exp(formula1, formula2){
-    return Number(calculate(formula1) ** Number(calculate(formula2)))
+    return Number(calculate(formula1) ** Number(calculate(formula2)));
 }
 
 function root(formula){
-    console.log(`root:${formula}`)
+    console.log(`root:${formula}`);
     if(Number(formula) < 0){
         console.log("minus root");
         errorflag = 1;
         return false;
     }
-    return Number(Math.sqrt(Number(formula)))
+    return Number(Math.sqrt(Number(formula)));
+}
+
+function solv_fac(num){
+    if (num === 0){
+        return 1;
+    }
+    return num * solv_fac(num-1);
+}
+
+function factorial(formula){
+    console.log(`factorial:${formula}`)
+    if(Number.isInteger(Number(formula)) && Number(formula) >= 0){
+        return solv_fac(formula);
+    }else{
+        errorflag = 1
+        return false;
+    }
+}
+
+function cal_sin(formula){
+    return Number(Math.sin(calculate(formula)));
+}
+
+function cal_cos(formula){
+    return Number(Math.cos(calculate(formula)));
 }
 
 function calculate(formula){
@@ -228,13 +330,28 @@ function calculate(formula){
         errorflag = 1;
         return false;
     }else if(brackets !== -1){
-        return calculate(formula.slice(0, brackets[0]) + calculate(formula.slice(brackets[0]+1, brackets[1])) + formula.slice(brackets[1]+1, formula.length)); 
+        if(brackets[0] > 0 && formula[brackets[0]-1] == "S"){
+            return calculate(formula.slice(0, brackets[0]-1) + cal_sin(formula.slice(brackets[0]+1, brackets[1])) + formula.slice(brackets[1]+1, formula.length));
+        }else if(brackets[0] > 0 && formula[brackets[0]-1] == "C"){
+            return calculate(formula.slice(0, brackets[0]-1) + cal_cos(formula.slice(brackets[0]+1, brackets[1])) + formula.slice(brackets[1]+1, formula.length));
+        }else{
+            return calculate(formula.slice(0, brackets[0]) + calculate(formula.slice(brackets[0]+1, brackets[1])) + formula.slice(brackets[1]+1, formula.length)); 
+  
+        }
     }
 
+    //ルート
     let roots = find_root(formula);
     console.log(`roots:${roots}`)
     if(roots !== -1){
         return calculate(formula.slice(0, roots[0]-1) + String(root(formula.slice(roots[0], roots[1])) + formula.slice(roots[1], formula.length)))
+    }
+
+    //階乗
+    let exps = find_exp(formula);
+    console.log(`exps:${exps}`)
+    if(exps !== -1){
+        return calculate(formula.slice(0, exps[0]) + String(factorial(formula.slice(exps[0], exps[1])) + formula.slice(exps[1]+1, formula.length)))
     }
 
     //加算。減算
@@ -278,10 +395,16 @@ function main(){
         return;
     }
     formula = pi(formula);
+    formula = napier(formula);
+    formula = trigonometric_function(formula);
     console.log(formula);
     formula = hiddenMul(formula);
     console.log(formula)
     formula = minus(formula);
+
+    if(formula.search(/^[\d|\+|\-|×|÷|\(|\)|\.|\^|√|!|~|S|C]*$/g) == -1){
+        errorflag = 1;
+    }
 
     //計算を実行
     result = calculate(formula);
